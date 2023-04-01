a42b6e5f46e514c3a5af1e801514ee59ffe335ce
kato atsushi <ka.baseball1997@gmail.com>
2023-04-01 15:36:24 +0900

    fix

--- src/api/lecture_customer.ts
+++ src/api/lecture_customer.ts
@@ -43,35 +43,79 @@ import {
   }
   
 
-  export type SimpleFavorite = {
+//   export type SimpleFavorite = {
+//     id: number;
+//     volume: number;
+//     eoa: string;
+//   };
+//   type FavoritesResponse = BaseResponse & {
+//     results: SimpleFavorite[];
+//   };
+
+//   export function useFetchFavoritesApi(): ApiSet<FavoritesResponse> & {
+//     execute: (accountAddress: string) => void;
+//   } {
+//     const api = useShowApi<FavoritesResponse>(new HttpClient(), {
+//       initialResponse: { results: [] },
+//     });
+
+//     const execute = (accountAddress: string): void => {
+//       const apiPath = `favorites/user_favorites/`;
+//       api.execute(apiPath, { accountAddress });
+//     };
+  
+//     return {
+//       ...api,
+//       isSuccess: () => !api.loading && !api.isError,
+//       execute: execute,
+//     };
+//   }
+
+
+  export type SimpleCustomer = {
     id: number;
     volume: number;
     eoa: string;
   };
   type LectureCustomerResponse = BaseResponse & {
-    results: SimpleFavorite[];
+    results: SimpleCustomer[];
   };
 
   export function useFetchLectureCustomerApi(): ApiSet<LectureCustomerResponse> & {
     execute: (form: LectureJoinInForm) => void;
   } {
-    const api = usePostRawApi<LectureCustomerResponse, LectureJoinInForm>(
-      new HttpClient(),
-      {
-        initialResponse: {},
-      },
-      { formatJson: true }
-    );
+    const api = useShowApi<LectureCustomerResponse>(new HttpClient(), {
+        initialResponse: { results: [] },
+    });
   
-    const execute = (form: LectureJoinInForm) => {
-      const apiPath = `lecture_customers/index/`;
-      console.log({勉強会の参加登録機能のフォーム: form})
-      api.execute(apiPath, form);
+    const execute = (accountAddress: string): void => {
+        const apiPath = `favorites/user_favorites/`;
+        api.execute(apiPath, { accountAddress });
     };
-  
+    
     return {
-      ...api,
-      isSuccess: () => !api.loading && !api.isError,
-      execute: execute,
+        ...api,
+        isSuccess: () => !api.loading && !api.isError,
+        execute: execute,
     };
+
+    // const api = usePostRawApi<LectureCustomerResponse, LectureJoinInForm>(
+    //   new HttpClient(),
+    //   {
+    //     initialResponse: {},
+    //   },
+    //   { formatJson: true }
+    // );
+  
+    // const execute = (form: LectureJoinInForm) => {
+    //   const apiPath = `lecture_customers/index/`;
+    //   console.log({勉強会の参加登録機能のフォーム: form})
+    //   api.execute(apiPath, form);
+    // };
+  
+    // return {
+    //   ...api,
+    //   isSuccess: () => !api.loading && !api.isError,
+    //   execute: execute,
+    // };
   }
